86bd1951a290ef73d5b5d646e6adb07e0b3e0208
Shiven <shivenkumar22@gmail.com>
2025-03-24 22:06:55 +0530

    bruh wtf

--- nasaimgotd/vite.config.js
+++ nasaimgotd/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_NASA_API_KEY': JSON.stringify(process.env.VITE_NASA_API_KEY)
+  }
+});
