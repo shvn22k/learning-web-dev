c76566f86b1f6157b76d3e4ee97baad438b936f1
Shiven <shivenkumar22@gmail.com>
2025-03-24 23:50:40 +0530

    stupid import fix

--- nasaimgotd/vite.config.js
+++ nasaimgotd/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: { 
-    'import.meta.env.VITE_NASA_API_KEY': JSON.stringify(process.env.VITE_NASA_API_KEY) 
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`
+  define: {
+    'import.meta.env.VITE_NASA_API_KEY': JSON.stringify(process.env.VITE_NASA_API_KEY)
   }
-});
+})
