532f32fa442dfd1b11de677d0717a677a2ee84d3
Shiven <shivenkumar22@gmail.com>
2025-03-24 22:22:05 +0530

    hell naw bruhh

--- nasaimgotd/src/App.jsx
+++ nasaimgotd/src/App.jsx
@@ -1,7 +1,9 @@
+import React from "react"; 
 import { useEffect, useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
+
 
 function App() {
   const [data, setData] = useState(null)
--- nasaimgotd/vite.config.js
+++ nasaimgotd/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  define: {
-    'import.meta.env.VITE_NASA_API_KEY': JSON.stringify(process.env.VITE_NASA_API_KEY)
+  plugins: [react()],
+  define: { 
+    'import.meta.env.VITE_NASA_API_KEY': JSON.stringify(process.env.VITE_NASA_API_KEY) 
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`
   }
 });
